409ad58f9679db44997af191d293e902f59df9d7
Ygor Salvalagio <salvalagio.ygor@gmail.com>
2022-02-24 00:13:27 -0300

    Include all routes in the Controller

--- Barbarian_Quest_API/Controllers/CharacterController.cs
+++ Barbarian_Quest_API/Controllers/CharacterController.cs
@@ -1,17 +1,67 @@
 ﻿using BQuest.Services.DTO;
+using BQuest.Services.Enumerators;
+using BQuest.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace Barbarian_Quest_API.Controllers
 {
     [Route("[Controller]")]
     public class CharacterController : Controller
     {
+        private readonly IConsultCharacterService _consultCharacterService;
+        private readonly ICreateCharacterService _createCharacterService;
+        private readonly IDeleteCharacterService _deleteCharacterService;
+        private readonly IAlterCharacterService _alterCharacterService;
+
+        public CharacterController(IConsultCharacterService consultInterface,
+                                   ICreateCharacterService createInterface,
+                                   IDeleteCharacterService deleteInterface,
+                                   IAlterCharacterService alterInterface)
+        {
+            _consultCharacterService = consultInterface;
+            _createCharacterService = createInterface;
+            _deleteCharacterService = deleteInterface;
+            _alterCharacterService = alterInterface;
+        }
+
         [HttpPost]
         [Route("Create")]
-        public IActionResult CreateCharacter([FromBody] Player player)
+        public IActionResult Create([FromBody] Player player)
+        {
+            Dictionary<int, object> playerValidation = player.Valid();
+
+            if (!(bool)playerValidation.GetValueOrDefault(1))
+                return BadRequest(new ErrorMessage($"One of the following attributes are wrong, please verify." + 
+                                    $"ps: try to verify these = {JsonSerializer.Serialize(playerValidation.GetValueOrDefault(2))}", player,null));
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("ConsultById")]
+        public IActionResult ConsultById(int Id)
         {
 
             return Ok();
         }
+
+        [HttpPut]
+        [Route("AlterAttributes")]
+        public IActionResult AlterAttributes([FromBody] Attributes attributes, string playerName)
+        {
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public IActionResult Delete(int Id)
+        {
+
+            return Ok();
+        }
+
     }
 }
